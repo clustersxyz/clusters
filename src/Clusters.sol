d1d3d76b1dad79c46ecf24c280917c8ad0fb4912
0xfoobar <0xfoobar1@gmail.com>
2023-10-31 17:07:32 -0700

    use utf-8 everywhere, cast to bytes32 in storage, add forward and reverse lookups for wallet names

--- src/Clusters.sol
+++ src/Clusters.sol
@@ -5,20 +5,15 @@ import {EnumerableSet} from "openzeppelin/contracts/utils/structs/EnumerableSet.
 
 import {Pricing} from "./Pricing.sol";
 
-// Each cluster has an ID
-// The ID is the unique identifier
-// The ID points to a set of addresses
-// Address can only be in one cluster at a time
 // Can a cluster have multiple names? Can it not have a name?
-// What should we call an address within a cluster?
-// Do we constrain names to be 32 bytes? (32 chars)
 // Where do we store expiries and how do we clear state?
 // It's actually more complex, user money is stored in escrow and can be used to pay harberger tax on loan or get outbid
 // And same for expiries, the bid is required to trigger. Need smooth mathematical functions here
-// Can users set a canonical name for cluster?
-// Should you be able to transfer name between clusters?
-// How do we handle when an account gets hacked and kick everyone else out from valuable cluster?
+// Can users set a canonical name for cluster? Yes, they can own multiple names and they can also have zero names.
+// Should you be able to transfer name between clusters? Yes, and how can they be traded?
+// How do we handle when an account gets hacked and kick everyone else out from valuable cluster? Problem of success, can just ignore. Don't get phished, 2FA not worth it.
 // What do we do about everybody being in cluster 0? Treat it like a burn address of sorts.
+// What does the empty foobar/ resolver point to? CREATE2 Singlesig?
 
 contract Clusters {
     using EnumerableSet for EnumerableSet.AddressSet;
@@ -32,10 +27,10 @@ contract Clusters {
     mapping(address addr => uint256 clusterId) public addressLookup;
 
     /// @notice Which cluster a name belongs to
-    mapping(string name => uint256 clusterId) public nameLookup;
+    mapping(bytes32 name => uint256 clusterId) public nameLookup;
 
     /// @notice Timestamp a name expires at
-    mapping(string name => uint256 expiry) public nameExpiry;
+    mapping(bytes32 name => uint256 expiry) public nameExpiry;
 
     /// @notice Enumerate all addresses in a cluster
     mapping(uint256 clusterId => EnumerableSet.AddressSet addrs) internal _clusterAddresses;
@@ -44,24 +39,62 @@ contract Clusters {
     mapping(uint256 clusterId => EnumerableSet.Bytes32Set names) internal _clusterNames;
 
     /// @notice Display name to be shown for a cluster, like ENS reverse records
-    mapping(uint256 clusterId => string name) public canonicalClusterName;
+    mapping(uint256 clusterId => bytes32 name) public canonicalClusterName;
 
+    /// @notice Outstanding invitations to join a cluster
     mapping(uint256 clusterId => mapping(address addr => bool invited)) public invited;
+
+    /// @notice For example lookup[17]["hot"] -> 0x123...
+    mapping(uint256 clusterId => mapping(bytes32 walletName => address wallet)) internal forwardLookup;
+
+    /// @notice For example lookup[0x123...] -> "hot", then combine with cluster name in a diff method
+    mapping(address wallet => bytes32 walletName) internal reverseLookup;
+
+    error MulticallFailed();
 
     constructor(address _pricing) {
         pricing = Pricing(_pricing);
     }
 
+    // TODO: Make this payable and pass along msg.value?
+    function multicall(bytes[] calldata data) external returns (bytes[] memory results) {
+        results = new bytes[](data.length);
+        bool success;
+        unchecked {
+            for (uint256 i = 0; i < data.length; ++i) {
+                //slither-disable-next-line calls-loop,delegatecall-loop
+                (success, results[i]) = address(this).delegatecall(data[i]);
+                if (!success) revert MulticallFailed();
+            }
+        }
+    }
+
     /// ECONOMIC FUNCTIONS ///
 
     function buyName(string memory name, uint256 clusterId, uint256 numSeconds) external payable {
-        // Check that name fits within bytes32
-        require(bytes(name).length <= 32, "name too long");
+        bytes32 name = _toBytes32(name);
         // Check that name is unused
         require(nameLookup[name] == 0, "name already bought");
         // TODO: issue refund
         require(msg.value >= pricing.getPrice(0.01 ether, numSeconds), "not enough eth");
         _assignName(name, clusterId, block.timestamp + numSeconds);
+    }
+
+    /// @notice Move name from one cluster to another without payment
+    function transferName(string memory name, uint256 toClusterId) external {
+        uint256 currentCluster = addressLookup[msg.sender];
+        require(_clusterNames[currentCluster].contains(_toBytes32(name)), "not name owner");
+        if (canonicalClusterName[currentCluster] == _toBytes32(name)) {
+            delete canonicalClusterName[currentCluster];
+        }
+        _clusterNames[currentCluster].remove(_toBytes32(name));
+        _clusterNames[toClusterId].add(_toBytes32(name));
+    }
+
+    function bidName(string memory name, uint256 clusterId) external payable {
+        // Deposit eth into escrow
+        uint256 bidAmount = msg.value;
+        // Should people have to precommit to time spent in escrow? No we want continuous
     }
 
     /// @dev Should work smoothly for fully expired names and names partway through their duration
@@ -75,9 +108,18 @@ contract Clusters {
     }
 
     function setCanonicalName(string memory name) external {
+        bytes32 name = _toBytes32(name);
         uint256 currentCluster = addressLookup[msg.sender];
         require(nameLookup[name] == currentCluster, "don't own name");
         canonicalClusterName[currentCluster] = name;
+    }
+
+    function setWalletName(string memory walletName) external {
+        bytes32 walletName = _toBytes32(walletName);
+        uint256 currentCluster = addressLookup[msg.sender];
+        require(forwardLookup[currentCluster][walletName] == address(0), "name already in use for cluster");
+        reverseLookup[msg.sender] = walletName;
+        forwardLookup[currentCluster][walletName] = msg.sender;
     }
 
     function invite(address invitee) external {
@@ -117,28 +159,34 @@ contract Clusters {
         invited[currentCluster][addr] = false;
     }
 
-    function _assignName(string memory name, uint256 clusterId, uint256 expiry) internal {
+    function _assignName(bytes32 name, uint256 clusterId, uint256 expiry) internal {
         nameLookup[name] = clusterId;
-        _clusterNames[clusterId].add(bytes32(bytes(name)));
+        _clusterNames[clusterId].add(name);
         nameExpiry[name] = expiry;
     }
 
     /// STRING HELPERS ///
 
+    function _toBytes32(string memory smallString) internal pure returns (bytes32 result) {
+        bytes memory smallBytes = bytes(smallString);
+        require(smallBytes.length <= 32, "name too long");
+        return bytes32(smallBytes);
+    }
+
     /// @dev Returns a string from a small bytes32 string.
-    function _fromSmallString(bytes32 smallString) internal pure returns (string memory result) {
-        if (smallString == bytes32(0)) return result;
+    function _toSmallString(bytes32 smallBytes) internal pure returns (string memory result) {
+        if (smallBytes == bytes32(0)) return result;
         /// @solidity memory-safe-assembly
         assembly {
             result := mload(0x40)
             let n
             for {} 1 {} {
                 n := add(n, 1)
-                if iszero(byte(n, smallString)) { break } // Scan for '\0'.
+                if iszero(byte(n, smallBytes)) { break } // Scan for '\0'.
             }
             mstore(result, n)
             let o := add(result, 0x20)
-            mstore(o, smallString)
+            mstore(o, smallBytes)
             mstore(add(o, n), 0)
             mstore(0x40, add(result, 0x40))
         }
--- src/Pricing.sol
+++ src/Pricing.sol
@@ -1,9 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.21;
 
-import {toWadUnsafe, wadExp, wadLn} from "solmate/utils/SignedWadMath.sol";
+import {toWadUnsafe, wadExp, wadLn, unsafeWadMul} from "solmate/utils/SignedWadMath.sol";
 
+/// @notice A stateless computation library for price, bids, decays, etc
+/// @dev All state is stored in clusters so we can replace the Pricing module while providing guarantees to existing holders
 contract Pricing {
+    uint256 internal constant SECONDS_IN_MONTH = 30 days;
     uint256 internal constant SECONDS_IN_YEAR = 365 days;
     uint256 internal constant DENOMINATOR = 10_000;
 
@@ -12,10 +15,16 @@ contract Pricing {
     function getPrice(uint256 p0, uint256 numSeconds) external pure returns (uint256) {
         return p0 * numSeconds / SECONDS_IN_YEAR + (15 * numSeconds ** 2) / (2 * SECONDS_IN_YEAR ** 2);
     }
-    
-    /// @notice Implements e^(-ln(0.5)x) ~= e^(-0.6931x) which cuts the number in half every year for exponential decay
-    /// @dev Watch out for rounding errors here if multiplier is <0.5
+
+    /// @notice Implements e^(ln(0.5)x) ~= e^(-0.6931x) which cuts the number in half every year for exponential decay
+    /// @dev Since this will be <1, returns a wad with 18 decimals
     function getDecayMultiplier(uint256 p0, uint256 numSecondsSinceBid) external pure returns (int256) {
         return wadExp(wadLn(0.5e18) * int256(numSecondsSinceBid) / int256(SECONDS_IN_YEAR));
+    }
+
+    /// @notice Should boost the annual price to 1/12th of (bidAmount * months)
+    function getBidMultiplier(uint256 p0, uint256 pBid, uint256 bidLengthInSeconds) external pure returns (int256) {
+        int256 wadMonths = toWadUnsafe(bidLengthInSeconds) / int256(SECONDS_IN_MONTH);
+        int256 targetPrice = unsafeWadMul(wadMonths, int256(pBid));
     }
 }
