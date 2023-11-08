0c9bf61bfd225e18bd459b7a6825d1eae840901f
0xfoobar <0xfoobar1@gmail.com>
2023-11-07 16:02:46 -0800

    cleanup:

--- src/Lambert.sol
+++ src/Lambert.sol
@@ -3,12 +3,10 @@ pragma solidity ^0.8.22;
 
 import {wadLn, unsafeWadMul, unsafeWadDiv} from "solmate/utils/SignedWadMath.sol";
 
-import {console2} from "forge-std/Test.sol";
-
 /// @notice Approximated principal branch of [Lambert W function](https://en.wikipedia.org/wiki/Lambert_W_function)
 /// @dev Only supports the [1/e, 3+1/e] and [3+1/e, inf] interval
-/// @dev Approximate [1/e, 3+1/e] with a lookup table weighted average and [3+1/e, inf] with ln(x) + ln(ln(x)) +
-/// ln(ln(x))/ln(x)
+/// @dev Approximate [1/e, 3+1/e] with a lookup table weighted average
+/// @dev Approximate and [3+1/e, inf] with ln(x) - ln(ln(x)) + ln(ln(x))/ln(x)
 contract Lambert {
     // int256 internal constant E_WAD = 2718281828459045235; // e
     int256 internal constant LOWER_BOUND_WAD = 367879441171442322; // 1/e
