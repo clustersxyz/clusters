dcb5f5311b45de73fa1b3e68abae05708d18122a
0xfoobar <0xfoobar1@gmail.com>
2023-12-01 11:05:56 -0500

    change github action for gas diff

--- test/GasBenchmark.t.sol
+++ test/GasBenchmark.t.sol
@@ -6,8 +6,28 @@ import {Clusters, NameManager} from "../src/Clusters.sol";
 import {Pricing} from "../src/Pricing.sol";
 import {IClusters} from "../src/IClusters.sol";
 
-contract ClustersTest is Test {
+contract GasBenchmarkTest is Test {
     Pricing public pricing;
     Clusters public clusters;
 
+    function setUp() public {
+        pricing = new Pricing();
+        clusters = new Clusters(address(pricing));
+        minPrice = pricing.minAnnualPrice();
+        vm.deal(address(this), 1 ether);
+    }
+
+    function _bytesToAddress(bytes32 _fuzzedBytes) internal pure returns (address) {
+        return address(uint160(uint256(keccak256(abi.encode(_fuzzedBytes)))));
+    }
+
+    function testBenchmark() public {
+        bytes32 callerSalt = "";
+        address caller = _bytesToAddress(_callerSalt);
+
+        vm.startPrank(caller);
+        clusters.create();
+        clusters.add(addr);
+        vm.stopPrank();
+    }
 }
