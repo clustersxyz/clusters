e5a59ee6c74016a9dace638ec66a1c64166de962
Zodomo <zodomo@proton.me>
2023-12-05 16:23:32 -0600

    finished initial spoke implementation

--- src/ClustersSpoke.sol
+++ src/ClustersSpoke.sol
@@ -7,7 +7,7 @@ import {EnumerableSet} from "../lib/openzeppelin-contracts/contracts/utils/struc
 
 import {NameManagerSpoke} from "./NameManagerSpoke.sol";
 
-import {IClusters} from "./IClusters.sol";
+import {IClusters, IEndpoint} from "./IClusters.sol";
 
 import {console2} from "../lib/forge-std/src/Test.sol";
 
@@ -20,75 +20,63 @@ import {console2} from "../lib/forge-std/src/Test.sol";
  * canonical name
  */
 
-interface IEndpoint {
-    function lzSend(
-        uint16 dstChainId,
-        address zroPaymentAddress,
-        bytes memory payload,
-        uint256 nativeFee,
-        bytes memory adapterParams
-    ) external;
-}
-
 contract ClustersHubMain is NameManagerSpoke {
     using EnumerableSet for EnumerableSet.AddressSet;
     using EnumerableSet for EnumerableSet.Bytes32Set;
-
-    bool internal _inMulticall;
 
     /// @dev Enumerate all addresses in a cluster
     mapping(uint256 clusterId => EnumerableSet.AddressSet addrs) internal _clusterAddresses;
 
     constructor(address pricing_, address endpoint_) NameManagerSpoke(pricing_, endpoint_) {}
 
-    /// EXTERNAL FUNCTIONS ///
+    /// USER-FACING FUNCTIONS ///
 
-    /// @dev For payable multicall to be secure, we cannot trust msg.value params in other external methods
-    /// @dev Must instead do strict protocol invariant checking at the end of methods like Uniswap V2
-    function multicall(bytes[] calldata data) external payable returns (bytes[] memory results) {
-        _inMulticall = true;
-        results = new bytes[](data.length);
-        bool success;
+    function multicall(bytes[] calldata data) external payable onlyEndpoint returns (bytes[] memory results) {
+        // Override logic if msg.sender is endpoint so only one multicall() function is necessary
+        if (msg.sender == endpoint) {
+            results = new bytes[](data.length);
+            bool success;
 
-        // Iterate through each call
-        for (uint256 i = 0; i < data.length; ++i) {
-            //slither-disable-next-line calls-loop,delegatecall-loop
-            (success, results[i]) = address(this).delegatecall(data[i]);
-            if (!success) revert MulticallFailed();
+            // Iterate through each call
+            for (uint256 i = 0; i < data.length; ++i) {
+                //slither-disable-next-line calls-loop,delegatecall-loop
+                (success, results[i]) = address(this).delegatecall(data[i]);
+                if (!success) revert MulticallFailed();
+            }
+
+            _checkInvariant();
+        } else {
+            // This is the logic executed by normal users
+            _inMulticall = true;
+            results = new bytes[](data.length);
+            bool success;
+
+            // Iterate through each call
+            for (uint256 i = 0; i < data.length; ++i) {
+                //slither-disable-next-line calls-loop,delegatecall-loop
+                (success, results[i]) = address(this).delegatecall(data[i]);
+                if (!success) revert MulticallFailed();
+            }
+
+            bytes memory payload = abi.encodeWithSignature("multicall(bytes[])", results, msg.sender);
+            IEndpoint(endpoint).lzSend(101, msg.sender, payload, msg.value, bytes(""));
+            _inMulticall = false;
         }
-
-        bytes memory payload = abi.encodeWithSignature("multicall(bytes[])", results, msg.sender);
-        IEndpoint(endpoint).lzSend(101, msg.sender, payload, msg.value, bytes(""));
-        _inMulticall = false;
     }
 
-    function multicall(bytes[] calldata data, address) external payable onlyEndpoint returns (bytes[] memory results) {
-        results = new bytes[](data.length);
-        bool success;
-
-        // Iterate through each call
-        for (uint256 i = 0; i < data.length; ++i) {
-            //slither-disable-next-line calls-loop,delegatecall-loop
-            (success, results[i]) = address(this).delegatecall(data[i]);
-            if (!success) revert MulticallFailed();
-        }
-
-        _checkInvariant();
-    }
-
-    function create() external payable returns (bytes memory payload) {
+    function create() public payable returns (bytes memory payload) {
         payload = abi.encodeWithSignature("create(address)", msg.sender);
         if (_inMulticall) return payload;
         else IEndpoint(endpoint).lzSend(101, msg.sender, payload, msg.value, bytes(""));
     }
 
-    function add(address addr) external payable returns (bytes memory payload) {
+    function add(address addr) public payable returns (bytes memory payload) {
         payload = abi.encodeWithSignature("add(address,address)", msg.sender, addr);
         if (_inMulticall) return payload;
         else IEndpoint(endpoint).lzSend(101, msg.sender, payload, msg.value, bytes(""));
     }
 
-    function remove(address addr) external payable returns (bytes memory payload) {
+    function remove(address addr) public payable returns (bytes memory payload) {
         payload = abi.encodeWithSignature("remove(address,address)", msg.sender, addr);
         if (_inMulticall) return payload;
         else IEndpoint(endpoint).lzSend(101, msg.sender, payload, msg.value, bytes(""));
@@ -100,17 +88,17 @@ contract ClustersHubMain is NameManagerSpoke {
 
     /// ENDPOINT FUNCTIONS ///
 
-    function create(address msgSender) external payable onlyEndpoint {
+    function create(address msgSender) public payable onlyEndpoint {
         _add(msgSender, nextClusterId++);
     }
 
-    function add(address msgSender, address addr) external payable onlyEndpoint {
+    function add(address msgSender, address addr) public payable onlyEndpoint {
         _checkZeroCluster(msgSender);
         if (addressToClusterId[addr] != 0) revert Registered();
         _add(addr, addressToClusterId[msgSender]);
     }
 
-    function remove(address msgSender, address addr) external payable onlyEndpoint {
+    function remove(address msgSender, address addr) public payable onlyEndpoint {
         _checkZeroCluster(msgSender);
         if (addressToClusterId[msgSender] != addressToClusterId[addr]) revert Unauthorized();
         _remove(addr);
