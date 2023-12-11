1a12465c334500724ff95924b1748788e8227530
Vectorized <webby1111@hotmail.com>
2023-12-11 17:58:52 +0000

    Optimize

--- src/EnumerableSetLib.sol
+++ src/EnumerableSetLib.sol
@@ -15,24 +15,24 @@ library EnumerableSetLib {
     struct Bytes32Set {
         mapping(uint256 => Bytes32) _values;
         mapping(bytes32 => Uint256) _positions;
-        bool _lengthInited;
-        uint248 _length;
+        uint256 _length;
     }
 
     function length(Bytes32Set storage set) internal view returns (uint256) {
-        if (!set._lengthInited) {
+        uint256 n = set._length;
+        if (n == 0) {
             if (set._values[0].value == bytes32(0)) return 0;
             if (set._values[1].value == bytes32(0)) return 1;
             if (set._values[2].value == bytes32(0)) return 2;
             return 3;
         }
-        return uint256(set._length);
+        return uint256(uint248(n));
     }
 
     function contains(Bytes32Set storage set, bytes32 value) internal view returns (bool) {
         if (value == bytes32(0)) revert ValueCannotBeZero();
 
-        if (!set._lengthInited) {
+        if (set._length == 0) {
             if (set._values[0].value == value) return true;
             if (set._values[1].value == value) return true;
             if (set._values[2].value == value) return true;
@@ -43,8 +43,8 @@ library EnumerableSetLib {
 
     function add(Bytes32Set storage set, bytes32 value) internal returns (bool result) {
         if (value == bytes32(0)) revert ValueCannotBeZero();
-
-        if (!set._lengthInited) {
+        uint256 n = set._length;
+        if (n == 0) {
             Bytes32 storage p0 = set._values[0];
             bytes32 v0 = p0.value;
             if (v0 == bytes32(0)) {
@@ -68,8 +68,7 @@ library EnumerableSetLib {
                 set._positions[v0].value = 1;
                 set._positions[v1].value = 2;
                 set._positions[value].value = 3;
-                set._lengthInited = true;
-                set._length = 3;
+                set._length = 3 | (1 << 255);
                 return true;
             } else if (v2 == value) {
                 return false;
@@ -78,13 +77,12 @@ library EnumerableSetLib {
         Uint256 storage q = set._positions[value];
         uint256 i = q.value;
         if (i == 0) {
-            uint256 n = uint256(set._length);
-            set._values[n].value = value;
+            set._values[uint256(uint248(n))].value = value;
             unchecked {
                 n += 1;
             }
-            q.value = n;
-            set._length = uint248(n);
+            q.value = uint256(uint248(n));
+            set._length = n;
             return true;
         }
     }
@@ -93,7 +91,8 @@ library EnumerableSetLib {
         if (value == bytes32(0)) revert ValueCannotBeZero();
 
         unchecked {
-            if (!set._lengthInited) {
+            uint256 n = set._length;
+            if (n == 0) {
                 Bytes32 storage p0 = set._values[0];
                 bytes32 v0 = p0.value;
                 Bytes32 storage p1 = set._values[1];
@@ -120,7 +119,8 @@ library EnumerableSetLib {
                 return false;
             }
             uint256 valueIndex = position - 1;
-            uint256 lastIndex = uint256(set._length) - 1;
+            uint256 j = n - 1;
+            uint256 lastIndex = uint256(uint248(j));
             Bytes32 storage last = set._values[lastIndex];
             if (valueIndex != lastIndex) {
                 bytes32 lastValue = last.value;
@@ -128,17 +128,19 @@ library EnumerableSetLib {
                 last.value = bytes32(0);
                 set._positions[lastValue].value = position;
             }
-            set._length = uint248(lastIndex);
+            set._length = j;
             p.value = 0;
             return true;
         }
     }
 
     function values(Bytes32Set storage set) internal view returns (bytes32[] memory result) {
-        uint256 n = length(set);
-        result = new bytes32[](n);
-        for (uint256 i; i != n; ++i) {
-            result[i] = set._values[i].value;
+        unchecked {
+            uint256 n = length(set);
+            result = new bytes32[](n);
+            for (uint256 i; i != n; ++i) {
+                result[i] = set._values[i].value;
+            }
         }
     }
 
