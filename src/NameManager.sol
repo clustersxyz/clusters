7e08ac40dd7b134f1886235f20d0aa6bf0de7c28
0xfoobar <0xfoobar1@gmail.com>
2023-12-14 11:19:45 -0500

    forge fmt

--- src/NameManager.sol
+++ src/NameManager.sol
@@ -144,7 +144,7 @@ abstract contract NameManager is IClusters {
             IClusters.PriceIntegral({lastUpdatedTimestamp: block.timestamp, lastUpdatedPrice: pricing.minAnnualPrice()});
         _assignName(_name, clusterId);
         if (defaultClusterName[clusterId] == bytes32("")) {
-            defaultClusterName[clusterId] = _name;  
+            defaultClusterName[clusterId] = _name;
             emit DefaultClusterName(_name, clusterId);
         }
         emit BuyName(_name, clusterId, msgValue);
