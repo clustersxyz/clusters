ba522569464016176e32a32496f836a18eafa172
Zodomo <zodomo@proton.me>
2024-01-17 14:41:20 -0600

    updated for holesky deployment

--- script/Testnet.s.sol
+++ script/Testnet.s.sol
@@ -11,62 +11,110 @@ import {IEndpoint} from "../src/interfaces/IEndpoint.sol";
 import {IOAppCore} from "layerzero-oapp/contracts/oapp/interfaces/IOAppCore.sol";
 import {ClustersHub} from "../src/ClustersHub.sol";
 
-contract ClustersScript is Script {
+contract TestnetScript is Script {
     address internal constant SIGNER = 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266;
     address internal constant LZ_END_GOERLI = 0x464570adA09869d8741132183721B4f0769a0287;
     address internal constant LZ_END_SEPOLIA = 0x464570adA09869d8741132183721B4f0769a0287;
+    address internal constant LZ_END_HOLESKY = 0x464570adA09869d8741132183721B4f0769a0287;
     uint32 internal constant LZ_EID_GOERLI = 40121;
     uint32 internal constant LZ_EID_SEPOLIA = 40161;
+    uint32 internal constant LZ_EID_HOLESKY = 40217;
 
-    string internal GOERLI_RPC_URL = vm.envString("GOERLI_RPC_URL");
     string internal SEPOLIA_RPC_URL = vm.envString("SEPOLIA_RPC_URL");
-    uint256 internal goerliFork;
+    string internal GOERLI_RPC_URL = vm.envString("GOERLI_RPC_URL");
+    string internal HOLESKY_RPC_URL = vm.envString("HOLESKY_RPC_URL");
     uint256 internal sepoliaFork;
+    uint256 internal goerliFork;
+    uint256 internal holeskyFork;
 
     function addressToBytes32(address addr) internal pure returns (bytes32) {
         return bytes32(uint256(uint160(addr)));
     }
 
     function setUp() public {
-        goerliFork = vm.createFork(GOERLI_RPC_URL);
         sepoliaFork = vm.createFork(SEPOLIA_RPC_URL);
+        goerliFork = vm.createFork(GOERLI_RPC_URL);
+        holeskyFork = vm.createFork(HOLESKY_RPC_URL);
     }
 
     function run() public {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
         address deployer = vm.addr(deployerPrivateKey);
 
-        vm.selectFork(goerliFork);
-        vm.startBroadcast(deployerPrivateKey);
-        PricingHarberger goerliPricing = new PricingHarberger();
-        IPricing goerliPricingProxy = IPricing(LibClone.deployERC1967(address(goerliPricing)));
-        PricingHarberger(address(goerliPricingProxy)).initialize(msg.sender, block.timestamp + 7 days);
-        Endpoint goerliEndpoint = new Endpoint();
-        IEndpoint goerliProxy = IEndpoint(LibClone.deployERC1967(address(goerliEndpoint)));
-        Endpoint(address(goerliProxy)).initialize(deployer, SIGNER, LZ_END_GOERLI);
-        ClustersHub goerliClusters =
-            new ClustersHub(address(goerliPricingProxy), address(goerliProxy), block.timestamp + 5 minutes);
-        goerliProxy.setClustersAddr(address(goerliClusters));
-        vm.stopBroadcast();
-
+        // Sepolia Hub Deployment
         vm.selectFork(sepoliaFork);
         vm.startBroadcast(deployerPrivateKey);
-        //PricingHarberger sepoliaPricing = new PricingHarberger(block.timestamp);
-        //IPricing sepoliaPricingProxy = IPricing(LibClone.deployERC1967(address(sepoliaPricing)));
-        //PricingHarberger(address(sepoliaPricingProxy)).initialize(msg.sender, block.timestamp + 7 days);
+        PricingHarberger sepoliaPricing = new PricingHarberger();
+        vm.label(address(sepoliaPricing), "Sepolia Pricing Template");
+        IPricing sepoliaPricingProxy = IPricing(LibClone.deployERC1967(address(sepoliaPricing)));
+        vm.label(address(sepoliaPricingProxy), "Sepolia Pricing Proxy");
+        PricingHarberger(address(sepoliaPricingProxy)).initialize(msg.sender, block.timestamp + 7 days);
         Endpoint sepoliaEndpoint = new Endpoint();
-        IEndpoint sepoliaProxy = IEndpoint(LibClone.deployERC1967(address(sepoliaEndpoint)));
-        Endpoint(address(sepoliaProxy)).initialize(deployer, SIGNER, LZ_END_SEPOLIA);
-        //ClustersHub sepoliaClusters = new ClustersHub(address(sepoliaPricingProxy), address(sepoliaProxy),
-        // block.timestamp);
-        //sepoliaProxy.setClustersAddr(address(sepoliaClusters));
-        IOAppCore(address(sepoliaProxy)).setPeer(LZ_EID_GOERLI, addressToBytes32(address(goerliProxy)));
-        sepoliaProxy.setDstEid(LZ_EID_GOERLI);
+        vm.label(address(sepoliaEndpoint), "Sepolia Endpoint Template");
+        IEndpoint sepoliaEndpointProxy = IEndpoint(LibClone.deployERC1967(address(sepoliaEndpoint)));
+        vm.label(address(sepoliaEndpointProxy), "Sepolia Endpoint Proxy");
+        Endpoint(address(sepoliaEndpointProxy)).initialize(deployer, SIGNER, LZ_END_SEPOLIA);
+        ClustersHub sepoliaClusters =
+            new ClustersHub(address(sepoliaPricingProxy), address(sepoliaEndpointProxy), block.timestamp + 5 minutes);
+        vm.label(address(sepoliaClusters), "Sepolia Clusters Hub");
+        sepoliaEndpointProxy.setClustersAddr(address(sepoliaClusters));
         vm.stopBroadcast();
 
+        // Goerli Spoke Deployment
         vm.selectFork(goerliFork);
         vm.startBroadcast(deployerPrivateKey);
-        IOAppCore(address(goerliProxy)).setPeer(LZ_EID_SEPOLIA, addressToBytes32(address(sepoliaProxy)));
+        //PricingHarberger goerliPricing = new PricingHarberger(block.timestamp);
+        //vm.label(address(goerliPricing), "Goerli Pricing Template");
+        //IPricing goerliPricingProxy = IPricing(LibClone.deployERC1967(address(goerliPricing)));
+        //vm.label(address(goerliPricingProxy), "Goerli Pricing Proxy");
+        //PricingHarberger(address(goerliPricingProxy)).initialize(msg.sender, block.timestamp + 7 days);
+        Endpoint goerliEndpoint = new Endpoint();
+        vm.label(address(goerliEndpoint), "Goerli Endpoint Template");
+        IEndpoint goerliEndpointProxy = IEndpoint(LibClone.deployERC1967(address(goerliEndpoint)));
+        vm.label(address(goerliEndpointProxy), "Goerli Endpoint Proxy");
+        Endpoint(address(goerliEndpointProxy)).initialize(deployer, SIGNER, LZ_END_GOERLI);
+        //ClustersHub goerliClusters = new ClustersHub(address(goerliPricingProxy), address(goerliEndpointProxy),
+        // block.timestamp);
+        //vm.label(address(goerliClusters), "Goerli Clusters Spoke");
+        //goerliEndpointProxy.setClustersAddr(address(goerliClusters));
+        IOAppCore(address(goerliEndpointProxy)).setPeer(LZ_EID_SEPOLIA, addressToBytes32(address(sepoliaEndpointProxy)));
+        goerliEndpointProxy.setDstEid(LZ_EID_SEPOLIA);
+        vm.stopBroadcast();
+
+        // Holesky Spoke Deployment
+        vm.selectFork(holeskyFork);
+        vm.startBroadcast(deployerPrivateKey);
+        //PricingHarberger holeskyPricing = new PricingHarberger(block.timestamp);
+        //vm.label(address(holeskyPricing), "Holesky Pricing Template");
+        //IPricing holeskyPricingProxy = IPricing(LibClone.deployERC1967(address(holeskyPricing)));
+        //vm.label(address(holeskyPricingProxy), "Holesky Pricing Proxy");
+        //PricingHarberger(address(holeskyPricingProxy)).initialize(msg.sender, block.timestamp + 7 days);
+        Endpoint holeskyEndpoint = new Endpoint();
+        vm.label(address(holeskyEndpoint), "Holesky Endpoint Template");
+        IEndpoint holeskyEndpointProxy = IEndpoint(LibClone.deployERC1967(address(holeskyEndpoint)));
+        vm.label(address(holeskyEndpointProxy), "Holesky Endpoint Proxy");
+        Endpoint(address(holeskyEndpointProxy)).initialize(deployer, SIGNER, LZ_END_HOLESKY);
+        //ClustersHub holeskyClusters = new ClustersHub(address(holeskyPricingProxy), address(holeskyEndpointProxy),
+        // block.timestamp);
+        //vm.label(address(holeskyClusters), "Holesky Clusters Spoke");
+        //holeskyEndpointProxy.setClustersAddr(address(holeskyClusters));
+        IOAppCore(address(holeskyEndpointProxy)).setPeer(LZ_EID_SEPOLIA, addressToBytes32(address(sepoliaEndpointProxy)));
+        IOAppCore(address(holeskyEndpointProxy)).setPeer(LZ_EID_GOERLI, addressToBytes32(address(goerliEndpointProxy)));
+        holeskyEndpointProxy.setDstEid(LZ_EID_SEPOLIA);
+        vm.stopBroadcast();
+
+        // Post-deployment Goerli config finalization
+        vm.selectFork(goerliFork);
+        vm.startBroadcast(deployerPrivateKey);
+        IOAppCore(address(goerliEndpointProxy)).setPeer(LZ_EID_HOLESKY, addressToBytes32(address(holeskyEndpointProxy)));
+        vm.stopBroadcast();
+
+        // Post-deployment Sepolia config finalization
+        vm.selectFork(sepoliaFork);
+        vm.startBroadcast(deployerPrivateKey);
+        IOAppCore(address(sepoliaEndpointProxy)).setPeer(LZ_EID_GOERLI, addressToBytes32(address(goerliEndpointProxy)));
+        IOAppCore(address(sepoliaEndpointProxy)).setPeer(LZ_EID_HOLESKY, addressToBytes32(address(holeskyEndpointProxy)));
+        // No DstEid is set on Sepolia Hub as that engages replication, which is not yet fully implemented
         vm.stopBroadcast();
     }
 }
