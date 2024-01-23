a01748325d050b4f1582b1b52037648d77cf07b7
0xfoobar <0xfoobar1@gmail.com>
2024-01-23 16:29:44 -0500

    wip

--- script/VanityMining.s.sol
+++ script/VanityMining.s.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.23;
 
 import {Script} from "forge-std/Script.sol";
 import {console2} from "forge-std/console2.sol";
+import {LibClone} from "solady/utils/LibClone.sol";
 import {BasicUUPSImpl} from "../src/BasicUUPSImpl.sol";
 
 interface ImmutableCreate2Factory {
@@ -20,7 +21,6 @@ interface ImmutableCreate2Factory {
 contract VanityMining is Script {
     ImmutableCreate2Factory immutable factory = ImmutableCreate2Factory(0x0000000000FFe8B47B3e2130213B802212439497);
     bytes initCode = type(BasicUUPSImpl).creationCode;
-    // bytes32 salt = 0x0000000000000000000000000000000000000000be6be37ac8fb6908565d0d53; // 0x1967ba51c0
     bytes32 salt = 0x000000000000000000000000000000000000000097e5b90d2f1f6025db407f4d; // 0x19670000000A93f312163Cec8C4612Ae7a6783b4
 
     function run() external {
@@ -29,8 +29,14 @@ contract VanityMining is Script {
         bytes32 initCodeHash = keccak256(initCode);
         console2.logBytes32(initCodeHash);
 
-        address implAddress = factory.safeCreate2(salt, initCode);
+        // address implAddress = factory.safeCreate2(salt, initCode);
+        address implAddress = 0x19670000000A93f312163Cec8C4612Ae7a6783b4;
         console2.log(implAddress);
+
+        address proxyAddress = LibClone.deployERC1967(implAddress);
+        console2.log(proxyAddress);
+
+
 
         vm.stopBroadcast();
     }
