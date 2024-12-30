481d1d1fa053516237f0020300ce560bfbf23407
Vectorized <webby1111@hotmail.com>
2024-12-30 16:03:49 +0000

    Add a very simple ClustersNFTBaseURIRenderer

---
+++ src/ClustersNFTBaseURIRenderer.sol
@@ -0,0 +1,54 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.23;
+
+import {LibString} from "solady/utils/LibString.sol";
+import {Ownable} from "solady/auth/Ownable.sol";
+
+/// @title ClustersNFTBaseURIRenderer
+/// @dev A very simple contract to return the token URI for the Clusters NFT,
+/// using a base URI and the token ID.
+contract ClustersNFTBaseURIRenderer is Ownable {
+    using LibString for LibString.StringStorage;
+
+    /*«-«-«-«-«-«-«-«-«-«-«-«-«-«-«-«-«-«-«-«-«-«-«-«-«-«-«-«-«-«-*/
+    /*                          STORAGE                           */
+    /*-»-»-»-»-»-»-»-»-»-»-»-»-»-»-»-»-»-»-»-»-»-»-»-»-»-»-»-»-»-»*/
+
+    LibString.StringStorage internal _baseURI;
+
+    /*«-«-«-«-«-«-«-«-«-«-«-«-«-«-«-«-«-«-«-«-«-«-«-«-«-«-«-«-«-«-*/
+    /*                           EVENTS                           */
+    /*-»-»-»-»-»-»-»-»-»-»-»-»-»-»-»-»-»-»-»-»-»-»-»-»-»-»-»-»-»-»*/
+
+    /// @dev The base URI is updated.
+    event BaseURISet(string baseURI);
+
+    /*«-«-«-«-«-«-«-«-«-«-«-«-«-«-«-«-«-«-«-«-«-«-«-«-«-«-«-«-«-«-*/
+    /*                        CONSTRUCTOR                         */
+    /*-»-»-»-»-»-»-»-»-»-»-»-»-»-»-»-»-»-»-»-»-»-»-»-»-»-»-»-»-»-»*/
+
+    constructor() {
+        _initializeOwner(tx.origin);
+    }
+
+    /*«-«-«-«-«-«-«-«-«-«-«-«-«-«-«-«-«-«-«-«-«-«-«-«-«-«-«-«-«-«-*/
+    /*                         TOKEN URI                          */
+    /*-»-»-»-»-»-»-»-»-»-»-»-»-»-»-»-»-»-»-»-»-»-»-»-»-»-»-»-»-»-»*/
+
+    /// @dev Returns the token URI for the given `id`.
+    function tokenURI(uint256 id) public view returns (string memory) {
+        string memory baseURI = _baseURI.get();
+        if (bytes(baseURI).length == 0) return "";
+        return LibString.replace(baseURI, "{id}", LibString.toString(id));
+    }
+
+    /*«-«-«-«-«-«-«-«-«-«-«-«-«-«-«-«-«-«-«-«-«-«-«-«-«-«-«-«-«-«-*/
+    /*                      ADMIN FUNCTIONS                       */
+    /*-»-»-»-»-»-»-»-»-»-»-»-»-»-»-»-»-»-»-»-»-»-»-»-»-»-»-»-»-»-»*/
+
+    /// @dev Sets the base URI for the token.
+    function setBaseURI(string memory baseURI) public onlyOwner {
+        _baseURI.set(baseURI);
+        emit BaseURISet(baseURI);
+    }
+}
